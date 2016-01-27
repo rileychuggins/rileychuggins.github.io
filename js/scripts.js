fbaa7cc785fd7977d1800066d49d97d44f0be07e
rileychuggins <riley.huggins@yahoo.com>
2016-01-26 19:37:08 -0500

    Form Scripts

--- js/scripts.js
+++ js/scripts.js
@@ -254,51 +254,51 @@ $(document).ready(function() {
 //        return false;
 //    });
 
-    $('.validate-required, .validate-email').on('blur change', function() {
-        validateFields($(this).closest('form'));
-    });
+//    $('.validate-required, .validate-email').on('blur change', function() {
+//        validateFields($(this).closest('form'));
+//    });
 
-    $('form').each(function() {
-        if ($(this).find('.form-error').length) {
-            $(this).attr('original-error', $(this).find('.form-error').text());
-        }
-    });
+//    $('form').each(function() {
+//        if ($(this).find('.form-error').length) {
+//            $(this).attr('original-error', $(this).find('.form-error').text());
+//        }
+//    });
 
-    function validateFields(form) {
-        var name, error, originalErrorMessage;
+//    function validateFields(form) {
+//        var name, error, originalErrorMessage;
 
-        $(form).find('.validate-required[type="checkbox"]').each(function() {
-            if (!$('[name="' + $(this).attr('name') + '"]:checked').length) {
-                error = 1;
-                name = $(this).attr('name').replace('[]', '');
-                form.find('.form-error').text('Please tick at least one ' + name + ' box.');
-            }
-        });
+//        $(form).find('.validate-required[type="checkbox"]').each(function() {
+//            if (!$('[name="' + $(this).attr('name') + '"]:checked').length) {
+//                error = 1;
+//                name = $(this).attr('name').replace('[]', '');
+//                form.find('.form-error').text('Please tick at least one ' + name + ' box.');
+//            }
+//        });
 
-        $(form).find('.validate-required').each(function() {
-            if ($(this).val() === '') {
-                $(this).addClass('field-error');
-                error = 1;
-            } else {
-                $(this).removeClass('field-error');
-            }
-        });
+//        $(form).find('.validate-required').each(function() {
+//            if ($(this).val() === '') {
+//                $(this).addClass('field-error');
+//                error = 1;
+//            } else {
+//                $(this).removeClass('field-error');
+//            }
+//        });
 
-        $(form).find('.validate-email').each(function() {
-            if (!(/(.+)@(.+){2,}\.(.+){2,}/.test($(this).val()))) {
-                $(this).addClass('field-error');
-                error = 1;
-            } else {
-                $(this).removeClass('field-error');
-            }
-        });
+//        $(form).find('.validate-email').each(function() {
+//            if (!(/(.+)@(.+){2,}\.(.+){2,}/.test($(this).val()))) {
+//                $(this).addClass('field-error');
+//                error = 1;
+//            } else {
+//                $(this).removeClass('field-error');
+//            }
+//        });
 
-        if (!form.find('.field-error').length) {
-            form.find('.form-error').fadeOut(1000);
-        }
+//        if (!form.find('.field-error').length) {
+//            form.find('.form-error').fadeOut(1000);
+//        }
 
-        return error;
-    }
+//        return error;
+//    }
 
 });
 
