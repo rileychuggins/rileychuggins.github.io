0b85ea7467fb6c2a325f21b359f63ebae25fbd2a
rileychuggins <riley.huggins@yahoo.com>
2016-01-26 20:01:29 -0500

    header & scripts revisions

--- js/scripts.js
+++ js/scripts.js
@@ -123,12 +123,12 @@ $(document).ready(function() {
 
     // Radio box controls
 
-//    $('.radio-holder').click(function() {
-//        $(this).siblings().find('input').prop('checked', false);
-//        $(this).find('input').prop('checked', true);
-//        $(this).closest('.radio-group').find('.radio-holder').removeClass('checked');
-//        $(this).addClass('checked');
-//    });
+    $('.radio-holder').click(function() {
+        $(this).siblings().find('input').prop('checked', false);
+        $(this).find('input').prop('checked', true);
+        $(this).closest('.radio-group').find('.radio-holder').removeClass('checked');
+        $(this).addClass('checked');
+    });
 
 //    $('form input[type="radio"]').each(function() {
 //        var valueText = $(this).closest('.radio-holder').find('span').text();
